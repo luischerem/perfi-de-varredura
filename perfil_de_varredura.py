257395dc6d85eb4714a4c60af635b1c2fa01fbb0
gabriel-almeidasilv <60849061+gabriel-almeidasilv@users.noreply.github.com>
2020-07-12 21:00:20 -0300

    Update perfil_de_varredura.py

--- perfil_de_varredura.py
+++ perfil_de_varredura.py
@@ -20,21 +20,16 @@ env.workspace = meu_ambiente
 if env.workspace:
     print("Ambiente: ", env.workspace)
 else:
-    print("É importante setar seu ambiente: C:\\users\\user\path\to\ambiente.gdb")
+    print("É importante setar seu ambiente. Exemplo: C:\\users\\user\path\to\ambiente.gdb")
     exit()
     
-#extrair a mascara
 out_extract_mask = ExtractByMask(in_raster, in_mask_data)
-#realizar o calculo a partir da mascara
 RE_mde_minus = out_extract_mask * -1
-#hydrology -> flow direction
 FD_mde_minus = arcpy.sa.FlowDirection(RE_mde_minus)
-#hydrology -> sink
 SK_mde_minus = arcpy.sa.Sink("FD_mde_minus")
-#converter raster para pontos
 pnt_mde_min  = arcpy.RasterToPoint_conversion(SK_mde_minus,
     meu_ambiente+"\\raster-point")
-#tentar extrair valores de pontos do raster
+
 try:
     pnt_mde_top1 = arcpy.sa.ExtractValuesToPoints(pnt_mde_min, in_raster,
         meu_ambiente+"\\pnt_mde_top1",
