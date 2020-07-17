3ebb801deca04cc9a91f5ffece86259eda645c48
gabriel <gabriel@mail.com>
2020-07-16 21:27:38 -0300

    novas modificações

--- perfil_de_varredura.py
+++ perfil_de_varredura.py
@@ -11,10 +11,13 @@ import os
 from arcpy.sa import *
 from arcpy import env
 
-#setar o ambiente de trabalho e variaveis
-in_raster     = "" #raster principal
-in_mask_data  = "" #poligono
+#setar variaveis de entrada
+in_raster     = "" #nome do raster principal
+in_mask_data  = "" #nome do poligono
 
+#teste de geoprocessamento dos dados
+#é importante apresentar de maneira correta o nome das variáveis
+#do contrario o script entrará em runtime error ou erro de execução
 try:
     outextract_mask = ExtractByMask(in_raster, in_mask_data)
     RE_mde_minus = out_extract_mask * -1
@@ -22,7 +25,17 @@ try:
     SK_mde_minus = arcpy.sa.Sink(FD_mde_minus)
     pnt_mde_min  = arcpy.RasterToPoint_conversion(SK_mde_minus, "pnt_mde_min")
     pnt_mde_top1 = arcpy.sa.ExtractValuesToPoints(pnt_mde_min, RE_mde_minus, "pnt_mde_top1")#passível de erro
-    pnt_mde_3d   = arcpy.FeatureTo3DByAttribute_3d("pnt_mde_top1", "pnt_mde_3d", "RASTERVALU")
+    pnt_mde_3d   = arcpy.FeatureTo3DByAttribute_3d(pnt_mde_top1, "pnt_mde_3d", "RASTERVALU")
 except arcpy.ExecuteError:
     print(arcpy.GetMessages())
     exit("Erro ao interpolar pontos!!, Verifique os arquivos!!")
+
+
+#def cleanTopographic():
+   #pass
+
+#def main():
+   #cleanTopographic():
+
+#if __name__ == "__main__":
+   #main()
